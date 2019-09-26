784f9c3d6c5198875d949710db02ecd524a43d11
Nasim Mahamad Shamem <43072049+nasimmahamadshamem@users.noreply.github.com>
2019-09-27 00:32:53 +0600

    Update LinearSearch.py

--- SearchAlgorithm/LinearSearch.py
+++ SearchAlgorithm/LinearSearch.py
@@ -1,1 +1,16 @@
-linear search
+def LinearSearch(arr,SearchData):
+    start = 0
+    end = len(arr)-1
+
+    for i in range(start,end):
+        if arr[i] == SearchData:
+            return f"Found index at {i+1}"
+    return "Not Found"
+
+
+if __name__ == '__main__':
+    arr = [24,45,434,23,423,423,23,423,423,4,56,657,435,33]
+
+    n = 423435
+
+    print(LinearSearch(arr,n))
