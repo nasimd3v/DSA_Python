aac44d361c2f2f72c76bf5ef50d7e7fa9627e363
Nasim Mahamad Shamem <43072049+nasimmahamadshamem@users.noreply.github.com>
2019-09-26 23:16:16 +0600

    Create Queue.py

---
+++ DataStructure/Queue.py
@@ -0,0 +1,22 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enQueue(self,data):
+        self.items.insert(0,data)
+
+    def deQueue(self):
+        self.items.pop()
+
+    def viewQueue(self):
+        print(self.items)
+
+q = Queue()
+q.enQueue("Shamem")
+q.enQueue("Mahamad")
+q.enQueue("Nasim")
+
+
+q.viewQueue()
+q.deQueue()
+q.viewQueue()
