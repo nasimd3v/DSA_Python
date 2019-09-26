0e609bd69cca79c1fadbe9aa3ae0aa20fccfa122
Nasim Mahamad Shamem <43072049+nasimmahamadshamem@users.noreply.github.com>
2019-09-27 00:00:38 +0600

    Update BinarySearch.py

--- SearchAlgorithm/BinarySearch.py
+++ SearchAlgorithm/BinarySearch.py
@@ -1,21 +1,32 @@
-def BinarySearch(arr,searchData):
 
+index = None
+
+def BinarySearch(arr,searchData):
+    
     start = 0
     end = len(arr)-1
 
-    while True:
+    while start <= end:
 
         mid = (start+end)//2
 
-        if searchData == arr[mid]:
-            print(f"Found ! Index: {mid+1}")
-            break
-    else:
-        if searchData < arr[mid]:
-            end = mid-1
+        if arr[mid] == searchData:
+            globals()['index'] = mid+1
+            return True
         else:
-            start = mid + 1
+            if searchData < arr[mid]:
+                end = mid-1
+            else:
+                start = mid + 1
+    return False
 
 if __name__ == '__main__':
-    arr = [1, 3, 5, 8, 9, 45, 56, 56, 56, 78, 4534, 34534]
-    BinarySearch(arr,45)
+
+    arr = [1, 3, 5, 8, 9, 45, 56, 78, 4534, 34534]
+
+    n = 50
+    
+    if BinarySearch(arr,n):
+        print(f'Found! Index: {index}')
+    else:
+        print(f"Oh Sorry! {n} Not Found")
